d0c3e1928a518b3c0366b8aa667ab114e518e735
Vincent <vincent@Vincents-Mac.local>
2016-01-24 09:13:11 -0800

    added file for suppercool

--- Suppereasy/ViewController.swift
+++ Suppereasy/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var boomlogo: UIImageView!
+    @IBOutlet weak var boombg: UIImageView!
+    @IBOutlet weak var uncool: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Makemekillyou(sender: AnyObject) {
+        boomlogo.hidden = false
+        boombg.hidden = false
+        uncool.hidden = true
+    }
 
 }
 
